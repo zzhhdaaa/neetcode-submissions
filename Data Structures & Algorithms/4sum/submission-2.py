9e6510e241935db2a9a9dcc36399eb5318c53d4f
Daaa Zhao <91817338+zzhhdaaa@users.noreply.github.com>
2026-05-21 12:44:22 -0700

    Add: 4sum - submission-2

---
+++ Data Structures & Algorithms/4sum/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+        
+        # loop down the first and second num, then it comes to a two sum II whereas two sum in a sorted array
+        for i in range(len(nums)):
+            if 0<=i-1<len(nums) and nums[i-1]==nums[i]:
+                continue
+
+            for j in range(i+1, len(nums)):
+                if i+1<=j-1<len(nums) and nums[j-1]==nums[j]:
+                    continue
+
+                # here we have two sum II, where we have a sorted array and find unique combinations
+                total = target - nums[j] - nums[i]
+                l = j+1
+                r = len(nums)-1
+                while l<r:
+                    if j+1<=l-1<len(nums) and nums[l-1] == nums[l]:
+                        l += 1
+                        continue
+                    summary = nums[l] + nums[r]
+                    if summary == total:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        l += 1
+                    elif summary < total:
+                        l += 1
+                    elif summary > total:
+                        r -= 1
+                
+        return res
+
+
