36291c9f9528f8393f9e351ac900f054117e7856
Daaa Zhao <91817338+zzhhdaaa@users.noreply.github.com>
2026-06-23 12:00:46 -0700

    Add: single-element-in-a-sorted-array - submission-2

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        # we only search through the idx 0, 2, 4, 6, ... len(nums)-1
+        l = 0
+        r = len(nums) // 2
+
+        while l<=r:
+            m = (l+r) // 2
+            idx = m*2
+            print(idx)
+            left = nums[idx-1] if idx-1>=0 else float('-inf')
+            middle = nums[idx]
+            right = nums[idx+1] if idx+1<len(nums) else float('inf')
+
+            if left < middle < right:
+                return middle
+            elif left == middle:
+                # in left portion
+                r = m
+            elif middle == right:
+                # in right portion
+                l = m+1
+
