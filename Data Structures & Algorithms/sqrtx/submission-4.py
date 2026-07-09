1868e8b7f5db1743ea2a2b2aefaf4d8a3e82ac6b
Daaa Zhao <91817338+zzhhdaaa@users.noreply.github.com>
2026-07-09 12:11:34 -0700

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+
+        while l < r:
+            m = (l+r)//2
+
+            a = m*m
+            b = (m+1)*(m+1)
+
+            if a <= x and b > x:
+                return m
+            elif a > x:
+                r = m
+            elif b <= x:
+                l = m + 1
+        
+        return l
